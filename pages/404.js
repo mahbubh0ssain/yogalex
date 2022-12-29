af97de17b567d677c034c99accfe178538546df4
Md. Mahbub Hossain <mahbubh0ssain.dev@gmail.com>
2022-12-29 12:08:41 +0600

    404 page added

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <img
+        className="min-h-[69.4vh]"
+        src="https://webdeasy.de/wp-content/uploads/2020/06/404-pages.jpg"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Error;
