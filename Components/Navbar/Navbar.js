dc64e1089e9d3ec25489143c168c76267288dcce
Md. Mahbub Hossain <mahbubh0ssain.dev@gmail.com>
2022-12-29 12:31:46 +0600

    git done

--- Components/Navbar/Navbar.js
+++ Components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { useContext } from "react";
-import { AuthContext } from "../pages/context/Authprovider";
+import { AuthContext } from "../../pages/context/Authprovider";
 
 const Navbar = () => {
   const { user, logOut } = useContext(AuthContext);
