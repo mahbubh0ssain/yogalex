58a57cef1805af053c38dd87ec7f5ca5df1387b9
Md. Mahbub Hossain <mahbubh0ssain.dev@gmail.com>
2023-01-22 02:04:44 +0600

    some designed added

--- Components/Header/Banner.js
+++ Components/Header/Banner.js
@@ -2,7 +2,7 @@ const Banner = () => {
   return (
     <>
       <img
-        className="w-100"
+        className="w-[75vw] h-[100vh]"
         src="https://i.ibb.co/HBFRSDp/patrick-malleret-p-v1-DBk-Trgo-unsplash-removebg-preview.png"
         alt=""
       />
--- Components/WhyShould/WhyShould.js
+++ Components/WhyShould/WhyShould.js
@@ -1,12 +1,12 @@
 const WhyShould = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 max-w-[1440px] mx-auto">
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 max-w-[1440px] mx-auto mb-16">
       <img
         className="hidden lg:block"
         src="https://i.ibb.co/Jrc6sGK/indian-yogi-yogi-madhav-8-Nt8-AR38-T0-I-unsplash.jpg"
         alt=""
       />
-      <div className="text-black flex flex-col justify-center p-6">
+      <div className="text-black flex flex-col justify-center px-6">
         <h1 className="font-thin text-6xl mb-3">Why You Should Go To Yoga</h1>
         <p className="text-2xl font-thin mb-3">
           Far far away, behind the word mountains, far from the countries
