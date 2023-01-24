d3e47df61d7979d24c60413b39981aa9acfbf662
Md. Mahbub Hossain <mahbubh0ssain.dev@gmail.com>
2023-01-24 12:53:41 +0600

    trainer route redesigned

--- Components/Trainer/Trainer.js
+++ Components/Trainer/Trainer.js
@@ -1,26 +1,34 @@
+import Link from "next/link";
 const Trainer = ({ trainer }) => {
   return (
-    <div>
-      <div className="card w-96 bg-base-100 shadow-xl mx-auto">
-        <figure>
-          <img src={trainer?.imgUrl} className="h-64 w-full" alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <span className="text-xl py-4 ">{trainer.name}</span>
+    <div className="card w-96 bg-base-100 shadow-xl mx-auto">
+      <figure>
+        <img
+          src={trainer?.imgUrl}
+          className="h-64 w-full"
+          alt="Trainer Image"
+        />
+      </figure>
+
+      <div className="card-body">
+        <div className="flex justify-between items-center border-b-2 pb-2">
+          <span className="text-xl pb-0 ">{trainer.name}</span>
           <div className="flex">
             <img
-              className="w-6 cursor-pointer mr-3"
+              className="w-6  h-6 cursor-pointer mr-3"
               src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
               alt=""
             />
             <img
-              className="w-6 cursor-pointer"
+              className="w-6 h-6  cursor-pointer"
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png"
               alt=""
             />
           </div>
         </div>
-        <button className="btn btn-primary mx-4 mb-4">Details</button>
+        <Link href={`/${trainer?._id}`}>
+          <button className="btn btn-primary w-full mt-4">Details</button>
+        </Link>
       </div>
     </div>
   );
---
+++ pages/[trainerId].js
@@ -0,0 +1,64 @@
+import Router from "next/router";
+
+const TrainerDetails = ({ trainer }) => {
+  const goBack = () => {
+    Router.back();
+  };
+  return (
+    <div className=" mx-6">
+      <div className="card bg-base-100 shadow-xl max-w-[768px] mx-auto">
+        <figure>
+          <img src={trainer?.imgUrl} className="h-64 w-full" alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <span className="text-xl py-4 border-b-2 pb-0 ">{trainer?.name}</span>
+          <div className="flex">
+            <img
+              className="w-6 cursor-pointer mr-3"
+              src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+              alt=""
+            />
+            <img
+              className="w-6 cursor-pointer"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png"
+              alt=""
+            />
+          </div>
+        </div>
+
+        <button onClick={goBack} className="btn btn-primary mx-4 mb-4">
+          Back
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TrainerDetails;
+
+export const getStaticProps = async (context) => {
+  const { params } = context;
+  const res = await fetch(`http://localhost:5000/trainer/${params?.trainerId}`);
+  const trainer = await res.json();
+  return {
+    props: {
+      trainer,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`http://localhost:5000/trainer`);
+  const trainers = await res.json();
+  const paths = trainers?.map((trainer) => {
+    return {
+      params: {
+        trainerId: `${trainer?._id}`,
+      },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
